fbb77a424b0442f609047f154681ef056248d778
coffeedeveloper <coffeedeveloper@gmail.com>
2013-09-07 22:54:09 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# 让IE7-8的input元素支持HTML5的placeholder
+## 使用方法
+	$('input[placeholder]').placeholder()
---
+++ jquery.placeholder.js
@@ -0,0 +1,44 @@
+﻿(function ($) {
+	$.fn.placeholder = function () {
+		return this.each(function () {
+			var $this = $(this);
+			switch($this.attr('type').toLowerCase()){
+				case 'text':
+						$this.data({
+							'placeholder': $this.attr('placeholder'),
+							'color': $this.css('color')
+						}).val($this.attr('placeholder')).css('color', '#999')
+						$this.focus(function () {
+							if ($(this).val() == $(this).data('placeholder'))
+								$(this).val('')
+							$(this).css('color', $(this).data('color'))
+						}).blur(function () {
+							if (!$(this).val() || $(this).val() == $(this).data('placeholder')) {
+								$(this).val($(this).data('placeholder')).css('color', '#999')
+							}
+						})
+					break;
+				case 'password':
+						var wrap = $('<label />')
+						wrap.css('position', 'relative')
+						var span = $('<span />')
+						span.css({
+							'position': 'absolute', 'left': $this.css('padding-left'),
+							'color': '#999', 'font-size': $this.css('font-size')
+						}).text($this.attr('placeholder')).attr('class', 'placeholder-text')
+						$this.wrap(wrap)
+						$this.after(span)
+						span.css({
+							'top' : '50%', 'margin-top':	span.height()/2 * -1
+						})
+						$this.focus(function () {
+							$(this).parent().find('.placeholder-text').hide()
+						}).blur(function () {
+							if (!$(this).val()) 
+								$(this).parent().find('.placeholder-text').show()
+						})
+					break;
+			}
+		})
+	}
+})(jQuery)
